eb411defaf11399d77d78ee89ddb7a18b1cf0e23
José Valim <jose.valim@dashbit.co>
2025-11-30 00:55:04 +0100

    Release v0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 # Changelog
 
-## v0.1.0-dev
+## v0.1.0 (2025-11-30)
 
 Initial release.
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NimbleZTA.MixProject do
   use Mix.Project
 
-  @version "0.1.0-dev"
+  @version "0.1.0"
   @repo_url "https://github.com/dashbitco/nimble_zta"
 
   def project do
